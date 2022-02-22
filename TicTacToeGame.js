45ae3df3c15f911ec084f2beaa01f095916f9a8f
Ridham2001 <70639096+Ridham2001@users.noreply.github.com>
2022-02-22 19:15:36 +0530

    Add first version

---
+++ TicTacToeGame.js
@@ -0,0 +1,128 @@
+function Board({ onClick, squares }) {
+    function renderSquare(i) {
+        return (
+            <button className="square" onClick={() => onClick(i)}>
+                {squares[i]}
+            </button>
+        );
+    }
+
+    return (
+        <div>
+            <div className="board-row">
+                {renderSquare(0)}
+                {renderSquare(1)}
+                {renderSquare(2)}
+            </div>
+            <div className="board-row">
+                {renderSquare(3)}
+                {renderSquare(4)}
+                {renderSquare(5)}
+            </div>
+            <div className="board-row">
+                {renderSquare(6)}
+                {renderSquare(7)}
+                {renderSquare(8)}
+            </div>
+        </div>
+    );
+}
+
+function Game() {
+    const [currentStep, setCurrentStep] = useLocalStorageState(
+        "tic-tac-toe:step",
+        0
+    );
+    const [history, setHistory] = useLocalStorageState("tic-tac-toe:history", [
+        Array(9).fill(null),
+    ]);
+
+    const currentSquares = history[currentStep];
+    const nextValue = calculateNextValue(currentSquares);
+    const winner = calculateWinner(currentSquares);
+    const status = calculateStatus(winner, currentSquares, nextValue);
+
+    function selectSquare(square) {
+        if (winner || currentSquares[square]) return;
+
+        const newHistory = history.slice(0, currentStep + 1);
+        const squaresCopy = [...currentSquares];
+        squaresCopy[square] = nextValue;
+
+        setHistory([...newHistory, squaresCopy]);
+        setCurrentStep(newHistory.length);
+    }
+
+    function restart() {
+        setHistory([Array(9).fill(null)]);
+        setCurrentStep(0);
+    }
+
+    const moves = history.map((stepSquares, step) => {
+        const desc = step === 0 ? "Go to game Start" : `Go to move #${step}`;
+        const isCurrentStep = step === currentStep;
+        return (
+            <li key={step}>
+                <button
+                    disabled={isCurrentStep}
+                    onClick={() => setCurrentStep(step)}
+                >
+                    {desc} {isCurrentStep ? "current" : null}
+                </button>
+            </li>
+        );
+    });
+
+    return (
+        <div className="game">
+            <div className="game-board">
+                <Board onClick={selectSquare} squares={currentSquares} />
+                <button className="restart" onClick={restart}>
+                    restart
+                </button>
+            </div>
+            <div className="game-info">
+                <div>{status}</div>
+                <ol>{moves}</ol>
+            </div>
+        </div>
+    );
+}
+
+function calculateStatus(winner, squares, nextValue) {
+    return winner
+        ? `Winner: ${winner}`
+        : squares.every(Boolean)
+        ? `Scratch: Cat's game`
+        : `Next player: ${nextValue}`;
+}
+
+function calculateNextValue(squares) {
+    return squares.filter(Boolean).length % 2 === 0 ? "X" : "O";
+}
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (
+            squares[a] &&
+            squares[a] === squares[b] &&
+            squares[a] === squares[c]
+        ) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+ReactDOM.render(<Game/>, document.getElementById("root"));
